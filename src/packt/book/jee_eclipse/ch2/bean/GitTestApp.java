27e4c3e8504b56fb9a8a0955a2b0a558f3ceb38b
Frank Liu <xliu65@hotmail.com>
2016-08-24 21:21:11 -0400

    frank branch - commit 01

---
+++ src/packt/book/jee_eclipse/ch2/bean/GitTestApp.java
@@ -0,0 +1,8 @@
+package packt.book.jee_eclipse.ch2.bean;
+
+public class GitTestApp {
+	public static void main(String[] args) {
+		System.out.println("Hello Git, from branch bug#1234 !!");
+		}
+
+}
